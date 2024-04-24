53089a0646610b468434808a6a9acb82f1516224
BeCare666 <becare.fr.ge@gmail.com>
2024-04-24 16:52:13 +0100

    sc

--- invest/index.js
+++ invest/index.js
@@ -24,99 +24,105 @@ const firebaseConfig = {
        InvestClass.forEach((T)=>{
       T.addEventListener('click', function(){
         var Amont = T.id
+        var valeurx = "5"
+        var Amonti = parseFloat(Amont);
+        var valeurxi = parseFloat(valeurx)
+        var sommes = Amonti + valeurxi
+        //console.log(sommes)
         if(!ACCOUNTINVESTSATUS && ACCOUNTINVEST ==0){
-       const newData = {
-         ACCOUNTINVEST: Amont,
-         };
-       userRef.update(newData, (error) => {
-         if (error){
+          const newData = {
+            ACCOUNTINVEST: sommes,
+            };
+          userRef.update(newData, (error) => {
+            if (error){
+              Swal.fire({
+                  title: "Ooops",
+                  text:"error",
+                  confirmButtonText: "OK",
+                  allowOutsideClick: false,
+                  icon: 'error'
+                  }).then((result)=>{
+                  if(result.isConfirmed){
+                      window.location.reload(); 
+                }
+               })
+            }else{
+        {/*          localStorage.setItem('SommesPrice', Amont)
+                       // Récupérer la valeur du champ de saisie
+                       const notificationidw = `Félicitations
+                       Votre pack d'investissement est  activé et nous vous informons 
+                       que vous venez de gagner 5$ que vous pouvez retirer sur votre 
+                       compte. Cette mesures es mise place pour s'assurer si votre compte 
+                       d'investissement a été bien configuré. Veillez retirer ces 5$ sous 
+                       peu. Merci de nous faire confiance.`
+        
+                       const dateActuelle = new Date();
+                       // Obtenez les composantes de la date et de l'heure  
+                       const jour = dateActuelle.getDate();
+                       const mois = dateActuelle.getMonth() + 1; // Les mois commencent à 0, donc ajoutez 1
+                       const annee = dateActuelle.getFullYear();
+                       const heures = dateActuelle.getHours();
+                       const minutes = dateActuelle.getMinutes();
+                       // Formatez la date et l'heure
+                       const dateFormatee = `${jour}/${mois}/${annee} ${heures}h:${minutes}min`;
+                     
+                       // Récupérez une référence à la liste des utilisateurs
+                       const usersRef = database.ref(`/utilisateurs/${userId}`);
+                       
+                       // Function to add a notification to the user's messages array
+                       function addNotificationToUser(userRef, notificationidw, status, time) {
+                           const newNotification = { notificationid: notificationidw, status: status, time: time };
+                           userRef.child("MESSAGES").push(newNotification);
+                       }              
+                       
+                       // Ajoutez la notification à tous les utilisateurs
+                       usersRef.once('value', function(snapshot) {
+                           snapshot.forEach(function(childSnapshot) {
+                               const userRef = childSnapshot.ref;
+                               addNotificationToUser(userRef, notificationidw, true, dateFormatee);
+                           });
+                 }); */}           
+                   Swal.fire({
+                       title: "Congratulation",
+                       text:"Your investment pack has been activated.",
+                       confirmButtonText: "OK",
+                       allowOutsideClick: false,
+                       icon: 'succes'
+                       }).then((result)=>{
+                       if(result.isConfirmed){
+                           window.location.reload(); 
+                   }
+                   })
+                   }
+               })
+        
+        }else if(!ACCOUNTINVESTSATUS && ACCOUNTINVEST !=0) {
            Swal.fire({
-               title: "Ooops",
-               text:"error",
+               title: "Information",
+               text:"You already have a pending investment package.",
                confirmButtonText: "OK",
                allowOutsideClick: false,
-               icon: 'error'
+               icon: 'info'
                }).then((result)=>{
                if(result.isConfirmed){
                    window.location.reload(); 
              }
             })
-         }else{
-{/*          localStorage.setItem('SommesPrice', Amont)
-                    // Récupérer la valeur du champ de saisie
-                    const notificationidw = `Félicitations
-                    Votre pack d'investissement est  activé et nous vous informons 
-                    que vous venez de gagner 5$ que vous pouvez retirer sur votre 
-                    compte. Cette mesures es mise place pour s'assurer si votre compte 
-                    d'investissement a été bien configuré. Veillez retirer ces 5$ sous 
-                    peu. Merci de nous faire confiance.`
-
-                    const dateActuelle = new Date();
-                    // Obtenez les composantes de la date et de l'heure  
-                    const jour = dateActuelle.getDate();
-                    const mois = dateActuelle.getMonth() + 1; // Les mois commencent à 0, donc ajoutez 1
-                    const annee = dateActuelle.getFullYear();
-                    const heures = dateActuelle.getHours();
-                    const minutes = dateActuelle.getMinutes();
-                    // Formatez la date et l'heure
-                    const dateFormatee = `${jour}/${mois}/${annee} ${heures}h:${minutes}min`;
-                  
-                    // Récupérez une référence à la liste des utilisateurs
-                    const usersRef = database.ref(`/utilisateurs/${userId}`);
-                    
-                    // Function to add a notification to the user's messages array
-                    function addNotificationToUser(userRef, notificationidw, status, time) {
-                        const newNotification = { notificationid: notificationidw, status: status, time: time };
-                        userRef.child("MESSAGES").push(newNotification);
-                    }              
-                    
-                    // Ajoutez la notification à tous les utilisateurs
-                    usersRef.once('value', function(snapshot) {
-                        snapshot.forEach(function(childSnapshot) {
-                            const userRef = childSnapshot.ref;
-                            addNotificationToUser(userRef, notificationidw, true, dateFormatee);
-                        });
-              }); */}           
-                Swal.fire({
-                    title: "Congratulation",
-                    text:"Your investment pack has been activated.",
-                    confirmButtonText: "OK",
-                    allowOutsideClick: false,
-                    icon: 'succes'
-                    }).then((result)=>{
-                    if(result.isConfirmed){
-                        window.location.reload(); 
-                }
-                })
-                }
+        }else if(ACCOUNTINVESTSATUS && ACCOUNTINVEST !=0){
+           Swal.fire({
+               title: "Information",
+               text:"You already have investment in progress.",
+               confirmButtonText: "OK",
+               allowOutsideClick: false,
+               icon: 'info'
+               }).then((result)=>{
+               if(result.isConfirmed){
+                   window.location.reload(); 
+             }
             })
-     
-    }else if(!ACCOUNTINVESTSATUS && ACCOUNTINVEST !=0) {
-        Swal.fire({
-            title: "Information",
-            text:"You already have a pending investment package.",
-            confirmButtonText: "OK",
-            allowOutsideClick: false,
-            icon: 'info'
-            }).then((result)=>{
-            if(result.isConfirmed){
-                window.location.reload(); 
-          }
-         })
-     }else if(ACCOUNTINVESTSATUS && ACCOUNTINVEST !=0){
-        Swal.fire({
-            title: "Information",
-            text:"You already have investment in progress.",
-            confirmButtonText: "OK",
-            allowOutsideClick: false,
-            icon: 'info'
-            }).then((result)=>{
-            if(result.isConfirmed){
-                window.location.reload(); 
-          }
-         })
-    
-     }
+        
+        }
+
     })
  
    })
@@ -137,3 +143,5 @@ cards.forEach((item) => {
     item.classList.add("active");
   });
 });
+
+
